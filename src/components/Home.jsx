0f2c692e1827fbf931c73e40f1c7723b14ebda97
Kennedy Carvalho <kennedy.carvalho@prof.infnet.edu.br>
2025-02-20 21:57:36 -0300

    Projeto v0.99

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,22 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Timer from './Timer';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 2rem;
+    min-height: 100vh;
+    background: #f8f9fa;
+    `;
+    
+    const Title = styled.h1`
+    font-size: 3rem;
+    color: #343a40;
+    margin-bottom: 1rem;
+  `;
 const Home = () => {
 
     const currDate = new Date();
@@ -8,12 +25,12 @@ const Home = () => {
     const toggleTimer = () => {setIsRunning(!isRunning)}
 
     return (
-        <>
-        <h1>Home</h1>
+        <Container>
+        <Title>Home</Title>
         The time now is {currDate.toLocaleTimeString()}
         <Timer isRunning={isRunning} />
         <button onClick={toggleTimer}>{isRunning?'Stop':'Start'}</button>
-        </>
+        </Container>
     );
 }
 
