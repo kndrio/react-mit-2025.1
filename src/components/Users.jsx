42e0ec9030d9badddf1720a03365f551be2ffcb5
Kennedy Carvalho <kennedy.carvalho@prof.infnet.edu.br>
2025-02-20 21:55:43 -0300

    Users refactor

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,38 +3,61 @@ import UserCard from "./UserCard";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://api.npoint.io/93ca752ec96d86449ded")
-      .then((response) => {
+
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("https://api.npoint.io/93ca752ec96d86449ded");
         if (!response.ok) {
           throw new Error("Erro ao buscar os dados");
         }
-        return response.json();
-      })
-      .then((data) => {
+        const data = await response.json();
         setUsers(data);
-        setLoading(false);
-      })
-      .catch((error) => {
+      }
+      catch (error){
         setError(error.message);
-        console.error("Sem resposta");
+        console.error("Sem resposta", error);
+      }
+      finally {
         setLoading(false);
-      });
+      }
+    }
+
+    fetchUsers();
+    
   }, []);
 
   const handleClick = (user) => {
     alert(`Nome: ${user.nome} \n Profissão: ${user.profissao}`);
   };
 
+
+  const filteredUsers = users.filter((user) =>
+
+    //user.nome.toLowerCase().includes(filter.toLowerCase())
+
+    Object.values(user).some(
+      (value) => String(value).toLowerCase().includes(filter.toLowerCase())
+    )
+  );
+
+
   if (loading) return <p>Carregando...</p>;
   if (error) return <p>Erro: {error}</p>;
 
   return (
     <div className="users">
-      {users.map((user, key) => (
+      <input
+        type="text"
+        placeholder="Buscar por nome..."
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      {filteredUsers.map((user, key) => (
         <UserCard key={key} user={user} onClick={() => handleClick(user)} />
       ))}
     </div>
